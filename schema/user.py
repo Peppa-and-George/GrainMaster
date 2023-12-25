a380174f6374256ef52b3fba05bdd97b32ed7bac
Li Wenkang <51111762+eye1218@users.noreply.github.com>
2023-12-25 20:42:14 +0800

    refactor: 模型拆分

--- schema/user.py
+++ schema/user.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import Column, Integer, String, DateTime, Float
+from sqlalchemy import Column, Integer, String, DateTime, Boolean
 
 from schema.database import Base
 
@@ -9,23 +9,9 @@ class User(Base):
     __tablename__ = "users"  # noqa
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    name = Column(String, index=True, nullable=False, comment="用户名")
-    phone_number = Column(Integer, nullable=False, comment="手机号")
-    hashed_passwd = Column(String)
+    name = Column(String, index=True, nullable=False, comment="用户名", name="name")
+    phone_number = Column(Integer, nullable=False, comment="手机号", name="phone_number")
+    hashed_passwd = Column(String, nullable=False, comment="密码", name="hashed_passwd")
 
     create_time = Column(DateTime, default=datetime.now)
     update_time = Column(DateTime, onupdate=datetime.now, default=datetime.now)
-
-
-class Product(Base):
-    __tablename__ = "product" # noqa
-
-    product_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    name = Column(String, index=True, nullable=False, comment="产品名称")
-    introduction = Column(String, index=True, nullable=False, comment="产品介绍")
-    price = Column(Integer, nullable=False, comment="价格")
-    unit = Column(String, index=True, nullable=False, comment="规格(L)")
-
-    create_time = Column(DateTime, default=datetime.now)
-    update_time = Column(DateTime, onupdate=datetime.now, default=datetime.now)
-

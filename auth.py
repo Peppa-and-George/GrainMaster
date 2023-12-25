825db35ff2e54a07b848763643883100cee71767
Li Wenkang <51111762+eye1218@users.noreply.github.com>
2023-12-25 20:44:46 +0800

    style: format

--- auth.py
+++ auth.py
@@ -4,14 +4,12 @@ from typing import Union
 from jose import jwt, JWTError
 from passlib.context import CryptContext
 
-
-
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 
 def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
     to_encode = data.copy()
@@ -23,12 +21,14 @@ def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
+
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, base64.b64decode(hashed_password).decode())
+
 
 def get_password_hash(password):
     return pwd_context.hash(password)
 
+
 def get_base64_password(password):
     return base64.b64encode(pwd_context.hash(password).encode()).decode()
-

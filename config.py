1d91a140b88a255992e69a7ca4036d958155c387
Li Wenkang <51111762+eye1218@users.noreply.github.com>
2024-03-08 16:23:10 +0800

    refactor: 静态资源路径修改

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 import os
 
 PRODUCT_DIR = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), "upload", "product_image"
+    os.path.dirname(os.path.abspath(__file__)), "upload", "images"
 )
 VIDEOS_DIR = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), "upload", "videos"
 )
 REPORT_DIR = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), "upload", "report"
+    os.path.dirname(os.path.abspath(__file__)), "upload", "reports"
 )
-FILE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "upload", "file")
+FILE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "upload", "files")
 if not os.path.exists(PRODUCT_DIR):
     os.makedirs(PRODUCT_DIR)
 if not os.path.exists(VIDEOS_DIR):
@@ -20,6 +20,6 @@ if not os.path.exists(FILE_DIR):
     os.makedirs(FILE_DIR)
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-IMAGE_DIR = r"upload/product_image"
+IMAGE_DIR = r"upload/images"
 
 BASE_URL = "http://13.215.201.101:8080"
